6eebc25911e117a932a7bd85335f7b0bd4600615
Daniel Deverell <daniel.deverell@gmail.com>
2021-09-15 17:53:10 -0400

    updated video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,13 +1,13 @@
 export default function Video() {
   return (
     <div className="gallery-container">
-      <h3 className="gallery-container special">An Actual Coaching Call </h3>
+      <h3 className="gallery-container special">Actual Coaching Call Sample</h3>
       <video
         poster="https://res.cloudinary.com/deedee/image/upload/v1631468707/real-estate/greg-call.png"
         className="flex"
         width="100%"
         height="100%"
-        src="https://videoproxy-production.s3.amazonaws.com/brlu68cobr2ovliojhpg/c4rs2t4fv9nbav2hp7bg/c4rs2t4fv9nbav2hp7bg.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIUQLRXLUOYMIIHEQ%2F20210912%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210912T173418Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=d5b4a11eafb06a8a80b8ec0b15b729757a236b7e2b85661ef93d9b19b4ec69ea"
+        src="https://res.cloudinary.com/deedee/video/upload/v1631742671/real-estate/asa-3.mp4"
         controls
       ></video>
     </div>
